a2f9f07ba29d6e3f7775ea3b32b4ce706f13335c
Marat Abrarov <abrarov@gmail.com>
2020-07-03 03:16:10 +0300

    Simplified init container script

--- init-image/src/main/resources/rootfs/run.sh
+++ init-image/src/main/resources/rootfs/run.sh
@@ -2,23 +2,15 @@
 
 set -e
 
-init() {
-  keystore_dir="$(dirname "${KEYSTORE_FILE}")"
-  if ! [ -d "${keystore_dir}" ]; then
-    mkdir -p "${keystore_dir}"
-  fi
+keystore_dir="$(dirname "${KEYSTORE_FILE}")"
+if ! [ -d "${keystore_dir}" ]; then
+  mkdir -p "${keystore_dir}"
+fi
 
-  openssl pkcs12 \
-    -export \
-    -inkey "${KEY_FILE}" \
-    -in "${CRT_FILE}" \
-    -name "${KEY_ALIAS}" \
-    -out "${KEYSTORE_FILE}" \
-    -password pass:"${KEYSTORE_PASSWORD}"
-}
-
-main() {
-  init "${@}"
-}
-
-main "${@}"
+openssl pkcs12 \
+  -export \
+  -inkey "${KEY_FILE}" \
+  -in "${CRT_FILE}" \
+  -name "${KEY_ALIAS}" \
+  -out "${KEYSTORE_FILE}" \
+  -password pass:"${KEYSTORE_PASSWORD}"
